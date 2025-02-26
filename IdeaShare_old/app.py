e09d57e8650239bde1bdc5b5d5f816acfda848a6
samKru123 <kruzliaksamuel4@gmail.com>
2025-02-26 21:16:20 +0100

    Add files via upload

--- IdeaShare_old/app.py
+++ IdeaShare_old/app.py
@@ -34,7 +34,7 @@ working_on = db.Table('working_on',
 
 
 
-# Funkcia na kontrolu správneho formátu súboru
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -44,31 +44,30 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)  # Len jeden môže mať True
+    is_admin = db.Column(db.Boolean, default=False)  
     is_category_admin = db.Column(db.Boolean, default=False)
-    is_blocked = db.Column(db.Boolean, default=False)  # Nový stĺpec pre blokovanie
+    is_blocked = db.Column(db.Boolean, default=False)  
     comments = db.relationship('Comment', backref='user', lazy=True, cascade="all, delete")
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
-    # Metóda na overenie hesla
+    
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
 class Idea(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    #category = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     comments = db.relationship('Comment', backref='idea', lazy=True, cascade="all, delete")
     upvotes = db.Column(db.Integer, default=0)
     categories = db.Column(db.String(255), nullable=True)
-    image = db.Column(db.String(255), nullable=True)  # Tu bude cesta k obrázku
+    file = db.Column(db.String(255), nullable=True)  
     working_users = db.relationship('User', secondary=working_on, backref=db.backref('working_ideas', lazy='dynamic'))
-    completed_by = db.Column(db.String(255), nullable=True)  # Uloží ID používateľov ako CSV
-    completion_link = db.Column(db.String(255), nullable=True)  # Link na hotový projekt
+    completed_by = db.Column(db.String(255), nullable=True)  
+    completion_link = db.Column(db.String(255), nullable=True)  
     completed_ideas = db.relationship('CompletedIdea', back_populates='idea', lazy=True, cascade="all, delete")
 
 class Comment(db.Model):
@@ -148,8 +147,7 @@ def login():
 
         if user:
             if user.is_blocked:
-                flash('Tento účet je zablokovaný!', 'danger')
-                return redirect(url_for('login'))
+                return redirect(url_for('home'))
             session['user_id'] = user.id
             session['is_admin'] = user.is_admin
             session['is_category_admin'] = user.is_category_admin
@@ -166,32 +164,40 @@ def logout():
 @app.route('/add_idea', methods=['GET', 'POST'])
 def add_idea():
     if 'user_id' not in session:
-        flash('Musíš byť prihlásený na pridanie nápadu.', 'danger')
         return redirect(url_for('login'))
 
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
-        selected_categories = request.form.getlist('categories')  # Získa vybrané kategórie
+        selected_categories = request.form.getlist('categories')  
         user_id = session['user_id']
         image_path = None
 
-        if 'image' in request.files and request.files['image'].filename != '':
-            file = request.files['image']
-            if file and allowed_file(file.filename):  
+        if 'file' in request.files:
+            file = request.files['file']
+            if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 file.save(file_path)
-                image_path = f'static/uploads/{filename}'
+                file_path_in_db = f'uploads/{filename}'  
+            else:
+                file_path_in_db = None
+        else:
+            file_path_in_db = None
 
-        
-        new_idea = Idea(title=title, description=description, user_id=user_id, categories=",".join(selected_categories), image=image_path)
+        new_idea = Idea(
+            title=title,
+            description=description,
+            user_id=user_id,
+            categories=",".join(selected_categories),
+            file=file_path_in_db  
+        )
 
         db.session.add(new_idea)
         db.session.commit()
         return redirect(url_for('home'))
 
-    # Načíta aktuálne kategórie z databázy
+    
     categories = Category.query.all()
     return render_template('add_idea.html', categories=categories)
 
@@ -199,17 +205,15 @@ def add_idea():
 @app.route('/admin')
 def admin():
     if 'user_id' not in session:
-        flash("Musíš byť prihlásený ako admin!", "danger")
         return redirect(url_for('login'))
 
     user = User.query.get(session['user_id'])
 
     if not user or not user.is_admin:
-        flash("Nemáš oprávnenie na správu kategórií!", "danger")
         return redirect(url_for('home'))
 
-    users = User.query.all()  # Načítanie všetkých používateľov
-    ideas = Idea.query.all()  # Načítanie všetkých nápadov
+    users = User.query.all()  
+    ideas = Idea.query.all()  
 
     return render_template('admin.html', users=users, ideas=ideas)
 
@@ -231,18 +235,15 @@ def idea_detail(idea_id):
 @app.route('/idea/<int:idea_id>/comment', methods=['POST'])
 def add_comment(idea_id):
     if not session.get('user_id'):
-        flash("Musíte byť prihlásený na pridanie komentára.", "danger")
         return redirect(url_for('login'))
 
     content = request.form['content']
     if not content.strip():
-        flash("Komentár nemôže byť prázdny!", "danger")
         return redirect(url_for('idea_detail', idea_id=idea_id))
 
     comment = Comment(content=content, idea_id=idea_id, user_id=session['user_id'])
     db.session.add(comment)
     db.session.commit()
-    flash("Komentár bol úspešne pridaný!", "success")
     return redirect(url_for('idea_detail', idea_id=idea_id))
 
 
@@ -250,7 +251,7 @@ def add_comment(idea_id):
 @app.route('/upvote/<int:idea_id>', methods=['POST'])
 def upvote_idea(idea_id):
     if 'user_id' not in session:
-        flash('You must be logged in to upvote.')
+        
         return redirect(url_for('login'))
 
     user_id = session['user_id']
@@ -260,14 +261,14 @@ def upvote_idea(idea_id):
         # kontrola ci uz uzivatel hlasoval
         existing_vote = Upvote.query.filter_by(user_id=user_id, idea_id=idea_id).first()
         if existing_vote:
-            flash('You have already upvoted this idea.', 'warning')
+            flash('uz si dal upvote.', 'warning')
         else:
             # pridanie hlasu
             upvote = Upvote(user_id=user_id, idea_id=idea_id)
             idea.upvotes += 1
             db.session.add(upvote)
             db.session.commit()
-            flash('Upvote added!', 'success')
+            
 
     return redirect(url_for('home'))
 
@@ -275,7 +276,6 @@ def upvote_idea(idea_id):
 @app.route('/idea/<int:idea_id>/work_on', methods=['POST'])
 def work_on_idea(idea_id):
     if 'user_id' not in session:
-        flash('Musíš sa prihlásiť, aby si mohol pracovať na nápade.', 'danger')
         return redirect(url_for('login'))
 
     user = User.query.get(session['user_id'])
@@ -283,10 +283,8 @@ def work_on_idea(idea_id):
 
     if user in idea.working_users:
         idea.working_users.remove(user)
-        flash('Nápad už nemáš označený ako rozpracovaný.', 'warning')
     else:
         idea.working_users.append(user)
-        flash('Úspešne si označil nápad ako rozpracovaný.', 'success')
 
     db.session.commit()
     return redirect(url_for('idea_detail', idea_id=idea_id))
@@ -296,7 +294,7 @@ def work_on_idea(idea_id):
 def user_profile(user_id):
     user = User.query.get_or_404(user_id)
     ideas_created = Idea.query.filter_by(user_id=user.id).all()
-    ideas_working_on = user.working_ideas  # Many-to-Many vzťah
+    ideas_working_on = user.working_ideas  
 
     return render_template('user_profile.html', user=user, ideas_created=ideas_created, ideas_working_on=ideas_working_on)
 
@@ -308,29 +306,26 @@ def user_profile(user_id):
 @app.route('/idea/<int:idea_id>/complete', methods=['POST'])
 def complete_idea(idea_id):
     if 'user_id' not in session:
-        flash('Musíš sa prihlásiť, aby si mohol označiť nápad ako dokončený.', 'danger')
         return redirect(url_for('login'))
 
     user_id = session['user_id']
     idea = Idea.query.get_or_404(idea_id)
     completion_link = request.form.get('completion_link', '').strip()
 
-    # Skontroluj, či už tento používateľ označil nápad ako dokončený
+    
     existing_completion = CompletedIdea.query.filter_by(user_id=user_id, idea_id=idea_id).first()
 
     if not existing_completion:
         completed_idea = CompletedIdea(user_id=user_id, idea_id=idea_id, completion_link=completion_link)
         db.session.add(completed_idea)
 
-        # ⚡ Odstránenie používateľa z "pracujem na tom"
+        
         if user_id in [user.id for user in idea.working_users]:
             idea.working_users.remove(User.query.get(user_id))
-            flash('Už viac nepracuješ na tomto nápade.', 'info')
 
         db.session.commit()
-        flash('Úspešne si označil nápad ako dokončený!', 'success')
     else:
-        flash('Tento nápad už máš označený ako dokončený.', 'warning')
+        flash('uz dokonceny.', 'warning')
 
     return redirect(url_for('idea_detail', idea_id=idea_id))
 
@@ -338,14 +333,12 @@ def complete_idea(idea_id):
 @app.route('/admin/block_user/<int:user_id>')
 def block_user(user_id):
     if 'user_id' not in session:
-        flash("Musíš byť prihlásený ako admin!", "danger")
         return redirect(url_for('login'))
 
     user = User.query.get(user_id)
     if user:
         user.is_blocked = True
         db.session.commit()
-        flash(f"Používateľ {user.username} bol zablokovaný.", "warning")
     
     return redirect(url_for('admin'))
 
@@ -358,11 +351,8 @@ def unblock_user(user_id):
     if user:
         user.is_blocked = False
         db.session.commit()
-        
     
     return redirect(url_for('admin'))
-
-
 
 
 @app.route('/delete_user/<int:user_id>', methods=['POST'])
@@ -411,48 +401,41 @@ def manage_categories():
 @app.route('/admin/delete_category/<int:category_id>', methods=['POST'])
 def delete_category(category_id):
     if 'user_id' not in session:
-        flash("Musíš byť prihlásený!", "danger")
         return redirect(url_for('login'))
 
     user = User.query.get(session['user_id'])
 
-    # Ochrana: Len category_admin môže mazať kategórie
+    
     if not user or not user.is_category_admin:
-        flash("Nemáš oprávnenie odstraňovať kategórie!", "danger")
         return redirect(url_for('home'))
 
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
     db.session.commit()
-    flash("Kategória bola odstránená.", "success")
     return redirect(url_for('manage_categories'))
 
 
 @app.route('/delete_own_idea/<int:idea_id>', methods=['POST'])
 def delete_own_idea(idea_id):
     if 'user_id' not in session:
-        flash("Musíte byť prihlásený!", "danger")
         return redirect(url_for('login'))
 
     idea = Idea.query.get_or_404(idea_id)
 
-    # Skontrolujeme, či používateľ vlastní nápad
+    
     if session['user_id'] != idea.user_id:
-        flash("Nemôžete zmazať nápad, ktorý vám nepatrí!", "danger")
         return redirect(url_for('home'))
 
-    # Zmažeme nápad aj s komentármi
+    
     Comment.query.filter_by(idea_id=idea.id).delete()
     db.session.delete(idea)
     db.session.commit()
     
-    flash("Váš nápad bol úspešne zmazaný!", "success")
     return redirect(url_for('home'))
 
 @app.route('/edit_own_idea/<int:idea_id>', methods=['GET', 'POST'])
 def edit_own_idea(idea_id):
     if 'user_id' not in session:
-        flash("Musíte byť prihlásený!", "danger")
         return redirect(url_for('login'))
 
     idea = Idea.query.get_or_404(idea_id)
